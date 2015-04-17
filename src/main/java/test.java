c2aeedecc1ef0b2e857d1f377c83c69475ceff3b
umishra <umishra@AML1493.eu.acncorp.com>
2015-04-17 14:43:42 +0200

    initial commit of gittest

---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+/**
+ * test created on 17/04/15
+ * <p/>
+ * Copyright (c) 2010, ACN and/or its affiliates. All rights reserved. ACN PROPRIETARY/CONFIDENTIAL. Use is subject to
+ * license terms.
+ */
+public class test {
+}
